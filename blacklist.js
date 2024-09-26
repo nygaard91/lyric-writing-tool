8c41a05fea5970480335c4291f97e62b7eed2c36
nygaard91 <56994335+nygaard91@users.noreply.github.com>
2024-09-26 18:45:09 +0200

    First upload
    First upload of existing project files

---
+++ blacklist.js
@@ -0,0 +1,48 @@
+// blacklist.js
+
+var blacklistText = `
+entwine
+unfold
+untold
+hold
+facade
+shadows
+skies
+sky
+done
+undone
+grand
+performance
+importance
+arise
+survive
+twisted
+game
+neon
+lights
+soil
+toil
+boil
+tapestry
+weaving
+twilight
+grace
+mend
+frail
+serenade
+divine
+sweet
+siren
+so fine
+bound
+blood
+whisper
+calls
+halls
+chains
+kiss
+bliss
+echoes
+`;
+
+var blacklist = blacklistText.trim().split(/\r?\n/);
